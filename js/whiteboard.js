e9886ecc774f8e14db1846f1818c3ad3b6bec3dd
holysmokas <holysmokasthatscheap@gmail.com>
2026-01-21 19:47:50 -0500

    change clear whiteboard icon + add share screen to all subscription tiers

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -70,10 +70,7 @@ const Whiteboard = (function () {
                 </div>
                 <div class="whiteboard-header-actions">
                     <button class="wb-header-btn danger" onclick="Whiteboard.clear()" title="Clear whiteboard">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                            <path d="M3 6h18"></path>
-                            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                        </svg>
+                        Clear
                     </button>
                     <button class="wb-header-btn" onclick="Whiteboard.close()" title="Close whiteboard">
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
